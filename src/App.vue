22c08fa456d8493b36fd816c1b068d67c46820b6
Nathan Daniel Peterson <natperson@gmail.com>
2019-03-06 06:12:17 -0800

    set up components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="container">
-    
+        <quote-grid :quotes="quotes"></quote-grid>
     </div>
 </template>
 
 <script>
+
+import QuoteGrid from './components/QuoteGrid.vue'
     export default {
+        data: function(){
+            return {
+                quotes: ['Placeholder quote'],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            quoteGrid: QuoteGrid
+        }
         
     }
 </script>
